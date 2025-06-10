d528435e821d059e62be0fffc46117137496ae2d
Pablo3141592654 <pablopetersen1229@gmail.com>
2025-06-10 07:28:18 +0200

    Update player.py

--- game/player.py
+++ game/player.py
@@ -1,10 +1,5 @@
 import random
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 593ef772e21a9b6be8b4fc1c42b4fa01be2fa9d1
 class Player:
     def __init__(self, color):
         self.color = color  # 1 for black, -1 for red
@@ -25,19 +20,11 @@ class GreedyGreta(Player):
 
 class MinimaxMax(Player):
     """Looks ahead a few moves, tries to maximize own pieces."""
-<<<<<<< HEAD
     def __init__(self, color, depths):
         super().__init__(color)
         index = 0 if color == 1 else 1
         self.depth = depths[index]
 
-    
-=======
-    def __init__(self, color, depth=2):
-        super().__init__(color)
-        self.depth = depth
-
->>>>>>> 593ef772e21a9b6be8b4fc1c42b4fa01be2fa9d1
     def get_move(self, board_obj):
         def minimax(board, player, depth):
             if depth == 0 or not board.has_valid_move(player):
