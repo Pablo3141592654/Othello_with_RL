3e8a1f8d97de9bcb9fd664b970720941840385ae
Pablo3141592654 <pablopetersen1229@gmail.com>
2025-06-20 16:55:23 +0200

    just put it back to old version (also not working) - probably because firebase...

--- app.py
+++ app.py
@@ -13,20 +13,22 @@ firebase_config = {
     "type": st.secrets["firebase"]["type"],
     "project_id": st.secrets["firebase"]["project_id"],
     "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "private_key": st.secrets["firebase"]["private_key"],
     "client_email": st.secrets["firebase"]["client_email"],
     "client_id": st.secrets["firebase"]["client_id"],
     "auth_uri": st.secrets["firebase"]["auth_uri"],
     "token_uri": st.secrets["firebase"]["token_uri"],
     "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
     "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["firebase"].get("universe_domain", "googleapis.com"),
+    "universe_domain": st.secrets["firebase"]["universe_domain"],
 }
 
+# Initialize Firebase app if not already initialized
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
+# Get Firestore client
 db = firestore.client()
 
 
@@ -105,7 +107,6 @@ def render_board(board):
             margin: 0;
         }}
     </style>
-
     <script>
         const clicked_id = "{clicked_id}"
         {firebase_js_config}
