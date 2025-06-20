12b463efba1557c5b36ed0ecb7110a53b1914575
Pablo3141592654 <pablopetersen1229@gmail.com>
2025-06-20 16:53:00 +0200

    try 3

--- app.py
+++ app.py
@@ -13,22 +13,20 @@ firebase_config = {
     "type": st.secrets["firebase"]["type"],
     "project_id": st.secrets["firebase"]["project_id"],
     "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
     "client_email": st.secrets["firebase"]["client_email"],
     "client_id": st.secrets["firebase"]["client_id"],
     "auth_uri": st.secrets["firebase"]["auth_uri"],
     "token_uri": st.secrets["firebase"]["token_uri"],
     "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
     "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["firebase"]["universe_domain"],
+    "universe_domain": st.secrets["firebase"].get("universe_domain", "googleapis.com"),
 }
 
-# Initialize Firebase app if not already initialized
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
-# Get Firestore client
 db = firestore.client()
 
 
