f00b9df07ac4c293c803ebe635e44570df508d80
Pablo3141592654 <pablopetersen1229@gmail.com>
2025-06-20 10:51:14 +0200

    Test

--- app.py
+++ app.py
@@ -58,14 +58,12 @@ def render_board(board):
     }};
     """
     
-    # interaction_attr = 'onclick="sendClick({i}, {j})"' if not st.session_state.get("rerun", False) else "onclick=''"
-
     board_html = ""
     for i in range(8):
         for j in range(8):
             cell = board[i][j]
             label = "⚫" if cell == 1 else "🔴" if cell == -1 else "⠀"
-            board_html += f'<div class="othello-cell" nclick="sendClick({i}, {j})">{label}</div>'
+            board_html += f'<div class="othello-cell" onclick="sendClick({i}, {j})">{label}</div>'
 
     full_html = f"""
     <script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js"></script>
